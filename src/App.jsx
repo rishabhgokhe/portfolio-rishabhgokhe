97381ec717e3993123dd236ea98ef1186bc0aa36
Rishabh Gokhe <rishabhgokhe2004@gmail.com>
2024-06-21 11:56:19 +0530

    fixed deployment issue on vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from 'react'
 import "./index.scss"
 import NavBar from './components/Layout/NavBar'
 import IntroSec from './components/IntroSec'
-import Features from './components/Features'
 import Footer from './components/Layout/Footer'
-
+import Features from './components/Features'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <div style={{textAlign: "center", background: "#DC143C", color: "white", padding: "2px"}} ><p>New Project Section Comming Soon !!! (React Version under development)</p></div>
