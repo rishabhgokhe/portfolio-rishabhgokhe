9343a54fec416888a91f1b719bb20a2a67108cf7
Rishabh Gokhe <rishabhgokhe2004@gmail.com>
2024-07-27 23:07:18 +0530

    fixing vercel deployment error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    distDir: '.next',
+  };
+  
+  export default nextConfig;
+  
